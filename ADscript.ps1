d0bbbabbe9a65d3768af063211224c1680aea31e
Dylan Dennison <71900262+IAmDylanDennison@users.noreply.github.com>
2021-06-14 09:50:15 -0400

    Add files via upload

---
+++ ADscript.ps1
@@ -0,0 +1,4 @@
+AD last login(<180 days)with useraccountcontrol= 512 script
+
+$date = (Get-Date).AddDays(-180)
+get-aduser -Filter { useraccountcontrol -eq 512 -and passwordlastset -LT $date} -properties * |select name, mail, passwordlastset, useraccountcontrol | Export-csv -path c:\ADInactivetest.csv
