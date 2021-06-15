3a0de1731639eb5711d42acb659db15751592517
Dylan Dennison <71900262+IAmDylanDennison@users.noreply.github.com>
2021-06-15 09:07:49 -0400

    Update ADscript.ps1

--- ADscript.ps1
+++ ADscript.ps1
@@ -1,4 +1,5 @@
 AD last login(<180 days)with useraccountcontrol= 512 script
 
 $date = (Get-Date).AddDays(-180)
-get-aduser -Filter { useraccountcontrol -eq 512 -and passwordlastset -LT $date} -properties * |select name, mail, passwordlastset, useraccountcontrol | Export-csv -path c:\ADInactivetest.csv
+get-aduser -Filter { useraccountcontrol -eq 512 -and passwordlastset -LT $date} -properties * |select name, mail, passwordlastset, useraccountcontrol | 
+Export-csv -path c:\ADInactivetest.csv
